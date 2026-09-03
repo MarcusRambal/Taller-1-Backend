301db0e0bcc45e27f96466aa333c60363f6c57c6
Marcus <marcusrambalpreston@gmail.com>
2026-09-03 13:13:38 -0500

    benchmarks de estrategia secuencial vs concurrente

--- peticiones.js
+++ peticiones.js
@@ -1,6 +1,6 @@
 //https://rickandmortyapi.com/api/character?pages=${}
 
-const resultadoDeApi = async () => {
+export const resultadoDeApi = async () => {
 
     const respuesta = await fetch("https://rickandmortyapi.com/api/character")
     const infoApi = await respuesta.json()
@@ -46,5 +46,5 @@ const resultadoDeApi = async () => {
 
 
 const todaslaspaginas = await resultadoDeApi()
-// console.log(todaslaspaginas)
+ console.log(todaslaspaginas)
 
