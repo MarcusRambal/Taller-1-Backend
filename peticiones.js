c64dde08671a30431ed1deaeb3af64f9024cc8e6
Marcus <marcusrambalpreston@gmail.com>
2026-09-03 11:29:29 -0500

    docs: Documentacion de como se consulta la api

--- peticiones.js
+++ peticiones.js
@@ -11,8 +11,13 @@ const resultadoDeApi = async () => {
     const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
     const blocks = 5    
+    
+    //Personajes contendra todos lo que se obtenga de la API en la parte de results, y se retornara al final de la funcion
     const personajes = []
 
+        // El primer for representa el primer valor del siguiente bloque de peticiones, y el segundo for representa cada una de las peticiones dentro del bloque
+        // Es decir el primer ciclo i es 1, pasado el bloque de 5, el siguiente ciclo i es 6 y así sucesivamente, mientras que el segundo ciclo j representa cada una  
+        // de las peticiones dentro del bloque, es decir 1,2,3,4,5 y así sucesivamente
         for (let i = 1; i <= numPag; i += blocks) {
             const peticiones = []
 
@@ -24,6 +29,7 @@ const resultadoDeApi = async () => {
             
             const resultados = await Promise.all(peticiones)
 
+            // Cada bloque lo agregamos al arreglo de personajes, y si no es el último bloque, esperamos 2 segundos antes de hacer el siguiente bloque de peticiones
             resultados.forEach(resultado => {
                 personajes.push(...resultado.results)
             })
@@ -41,3 +47,4 @@ const resultadoDeApi = async () => {
 
 const todaslaspaginas = await resultadoDeApi()
 // console.log(todaslaspaginas)
+
