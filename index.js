0ac3ee0ae1f171f4852c0677b6d02c557ba3d5b1
AlexDanii <127868978+AlexDanii@users.noreply.github.com>
2026-09-04 02:43:52 -0500

    cambios locales antes de actualizar rama

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+// index.js
+import { normalizarDatos } from './normalizacion.js';
+import { ejecutarConsultas } from './consultas.js';
+
+console.log('Obteniendo y normalizando personajes...');
+const personajesNormalizados = await normalizarDatos();
+
+console.log('Ejecutando consultas...');
+const resultados = ejecutarConsultas(personajesNormalizados);
+
+console.log('--- RESULTADOS ---');
+console.log(resultados);
